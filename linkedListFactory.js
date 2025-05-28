ecb0ce5bc2eb08ad86ae539d6ca8269401463912
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-05-28 10:56:08 -1000

    Add a method for finding linked list tail

--- linkedListFactory.js
+++ linkedListFactory.js
@@ -4,4 +4,22 @@ export class LinkedList {
     constructor() {
         this.head = null;
     }
+
+    append(data) {
+
+    }
+
+    tail() {
+        if (this.head === null) {
+            return null
+        }
+        
+        let currentPosition = this.head;
+
+        while (currentPosition.nextNode != null) {
+            currentPosition = currentPosition.nextNode;
+        }
+
+        return currentPosition;
+    }
 }
